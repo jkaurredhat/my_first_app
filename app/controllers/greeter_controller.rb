d39248f663a405604bb5b1f2e935d17712f1d2e4
Sachin Patil <sacpatil@redhat.com>
2016-03-11 12:51:58 +0530

    Move business login out of view to the controller

--- app/controllers/greeter_controller.rb
+++ app/controllers/greeter_controller.rb
@@ -1,5 +1,12 @@
 class GreeterController < ApplicationController
   def hello
+    random_names = ["Alex", "Joe", "Michael"]
+    # Variable starting with '@' is instance variable.
+    # For every request, instance variable is newly generated.
+    @name = random_names.sample
+    @time = Time.now
+    @times_displayed ||= 0
+    @times_displayed += 1
   end
 
   def goodbye
